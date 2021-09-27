865f601d707e571bec8fb5bae455b47e305225cf
mauronvrr19 <mauronvrr19@gmail.com>
2021-09-27 18:39:37 -0300

    repositorio

--- java.js
+++ java.js
@@ -1,22 +1,21 @@
-const euro = (x) => {
-    alert(`la cotizacion es ${x} *  ${311} = ${x * 311}`)
+const euro = (x) => {           // defini la funcion para cotizar euros
+    alert(`la cotizacion es ${x} *  ${311} = E ${x * 311} `)
     return x * 311;
 }
-
-const dolar = (x) => {
-    alert(`la cotizacion es ${x} *  ${185} = ${x * 185}`)
+const dolar = (x) => {         // defini la funcion para cotizar dolar
+    alert(`la cotizacion es ${x} *  ${185} = US ${x * 185}`)
     return x * 185;
 }
 
-const real = (x) => {
-    alert(`la cotizacion es ${x} *  ${33} = ${x * 33}`)
+const real = (x) => {            // defini la funcion para cotizar euro
+    alert(`la cotizacion es ${x} *  ${33} =  R ${x * 33}`)
     return x * 33;
 }
 
-const main = (texto = "Ingresa el numero de la divisa que desea comprar") => {
-    let opcion = Number(prompt(
-        ` ${texto}
-
+const main = () => {         // menu para eligir la cotizacion
+ let opcion = Number(prompt(
+        ` 
+elija el numero de la cotizacion que desea calcular
 	1 - euro
 	2 - dolar
 	3 - real
@@ -26,18 +25,16 @@ const main = (texto = "Ingresa el numero de la divisa que desea comprar") => {
 	`));
     return opcion;
 }
-let menu = main();
+let menu = main();   
 
-let x;
 while (menu != 0) {
-
-
+    let x ;
     switch (menu) {
         case 1:
             alert("la cotizacion del euro blue es de 211")
             x = Number(prompt("Ingrese el monto a comprar"));
             if (x <= 200) {
-                alert(`El resultado de la conversion es = ${x * 211}`)
+                euro(x);
             } else {
                 alert("disculpe solo podemos vender 200 euros por normativa de la bcra")
             }
@@ -49,7 +46,7 @@ while (menu != 0) {
             alert("la cotizacion del euro blue es de 185")
             x = Number(prompt("Ingrese el monto a comprar"));
             if (x <= 200) {
-                alert(`El resultado de la conversion es = ${x * 185}`)
+                dolar(x)
             } else {
                 alert("disculpe solo podemos vender 200 euros por normativa de la bcra")
             }
@@ -60,17 +57,18 @@ while (menu != 0) {
             alert("la cotizacion del euro blue es de 33")
             x = Number(prompt("Ingrese el monto a comprar"));
             if (x <= 200) {
-                alert(`El resultado de la conversion es = ${x * 33}`)
-            } else {
-                alert("disculpe solo podemos vender 200 euros por normativa de la bcra")
+             real(x)
             }
-
-            menu = main();
-            break;
-
-
-        default:
-            menu = main;
-            break;
+            else {
+        alert("disculpe solo podemos vender 200 euros por normativa de la bcra")
     }
+
+    menu = main();
+    break;
+
+
+    default:
+    menu = main();
+    break;
+}
 }
